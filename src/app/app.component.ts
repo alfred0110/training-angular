becdd68dba7bd64618072b2ebbadc6a8363482f8
alfred0110 <alf.nava.mend@gmail.com>
2021-06-21 14:34:11 -0500

    6 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,22 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  
+  registerForm: FormGroup;
+  submitted:boolean= false;
+
+  constructor( private formBuilder: FormBuilder){}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: ['',{updateOn: 'blur', Validators: Validators.required}],
+        lastName: ['', Validators.required],
+        address: this.formBuilder.group({
+          street:[],
+          zip:[],
+          city:[]
+        })
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule Re } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent, RegistrationFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
